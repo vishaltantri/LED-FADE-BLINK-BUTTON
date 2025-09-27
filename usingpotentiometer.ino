df18570e0ce5576707f045be2d0ededd161af367
vishaltantri <tantrivishal@gmail.com>
2025-09-27 21:25:03 +0530

    added use of map fn

--- usingpotentiometer.ino
+++ usingpotentiometer.ino
@@ -8,6 +8,7 @@ void loop() {
   int sensorValue = analogRead(A0);
   float voltage = sensorValue * (5.0 / 1023.0);//arduino supports upto 5v only
   int brightness = sensorValue / 4; //brightness is upto 256 only
+  //int brightness=map(sensorValue,0,1023,0,255); u can also use this to change range that is from 0-1023 to 0-255
   // 1023 / 4 = 255.75 but the .75 cuts off
   analogWrite(11, brightness);
   // print out the value you read:
